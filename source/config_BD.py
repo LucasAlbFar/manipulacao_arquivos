646ec97fcd3421279959d4a7547059074abcba62
Lucas Albuquerque de Faria <39134983+LucasAlbFar@users.noreply.github.com>
2021-09-01 11:21:13 -0300

    Add files via upload

--- source/config_BD.py
+++ source/config_BD.py
@@ -2,5 +2,5 @@ from sqlalchemy import create_engine
 
 
 def criar_engine():
-    engine = create_engine('postgresql+psycopg2://postgres:noadmin@localhost:5432/dados_validos')
+    engine = create_engine('postgresql+psycopg2://username:password@localhost:5432/database')
     return engine
